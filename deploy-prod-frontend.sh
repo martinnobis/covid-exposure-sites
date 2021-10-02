aa825defa4917964c9c2dd2b9ccd7ee181281f06
Martin Nobis <martin.nobis@gmail.com>
2021-10-02 20:20:53 +1000

    Update deploy-prod script.

--- deploy-prod-frontend.sh
+++ deploy-prod-frontend.sh
@@ -1,44 +1,16 @@
 #!/bin/sh
 
-# uglifyjs script.js -m toplevel -c -o public/script.min.js -c drop_console
-cp dist/index.bundle.js dist/main.css public/
+rm -rf public/*.html
+rm -rf public/*.js
+rm -rf public/*.LICENSE
 
-html-minifier \
-    --collapse-whitespace \
-    --remove-comments \
-    --remove-optional-tags \
-    --remove-redundant-attributes \
-    --remove-script-type-attributes \
-    --remove-tag-whitespace \
-    --use-short-doctype \
-    --minify-css true \
-    --minify-js true \
-    src/index.html -o public/index.html
+# JS
+cp dist/index.*.bundle.js public/
+cp dist/index.*.bundle.js.LICENSE.txt public/
+cp dist/privacypolicy.*.bundle.js public/
+cp dist/acceptableuse.*.bundle.js public/
 
-html-minifier \
-    --collapse-whitespace \
-    --remove-comments \
-    --remove-optional-tags \
-    --remove-redundant-attributes \
-    --remove-script-type-attributes \
-    --remove-tag-whitespace \
-    --use-short-doctype \
-    --minify-css true \
-    --minify-js true \
-    src/privacypolicy.html -o public/privacypolicy.html
-
-html-minifier \
-    --collapse-whitespace \
-    --remove-comments \
-    --remove-optional-tags \
-    --remove-redundant-attributes \
-    --remove-script-type-attributes \
-    --remove-tag-whitespace \
-    --use-short-doctype \
-    --minify-css true \
-    --minify-js true \
-    src/acceptableuse.html -o public/acceptableuse.html
-
-# cssnano < style.css > public/style.min.css
-
-# rm -f public/style.css public/script.js
+# HTML
+cp dist/index.html public/
+cp dist/privacypolicy.html public/
+cp dist/acceptableuse.html public/
